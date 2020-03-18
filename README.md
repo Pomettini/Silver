6e03756c256c1f2d471f40474fed92e2def57074
Giorgio Pomettini <giorgio.freespire@gmail.com>
2020-03-18 15:50:24 +0100

    Added brushes and ES6 magic

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Silver
+
 Stupidly sImple LeVel Editor for Roguelikes
+
+Everything is brutally hardcoded for now, so use at your risk
--- js/silver.js
+++ js/silver.js
@@ -1,9 +1,43 @@
+// --- Constants ---
+
 const W_CELLS = 19;
 const H_CELLS = 11;
 const CELL_SIZE = 50;
 
+const BRUSH_TYPES = {
+  "None": {
+    id: 0,
+    color: "white",
+    textcolor: "black"
+  },
+  "Void": {
+    id: 1,
+    color: "darkslategray",
+    textcolor: "white"
+  },
+  "Ground": {
+    id: 2,
+    color: "peru",
+    textcolor: "black"
+  },
+  "Chest": {
+    id: 3,
+    color: "yellow",
+    textcolor: "black"
+  }
+}
+
+// --- Variables ---
+
+var IsMouseDown = false;
+var CurrentBrushType = BRUSH_TYPES["None"];
+
+// --- Functions ---
+
 $(function () {
   console.log("Ready!");
+
+  AddBrushes();
 
   $("#grid").width(W_CELLS * CELL_SIZE);
   $("#grid").height(H_CELLS * CELL_SIZE);
@@ -14,19 +48,60 @@ $(function () {
   $(".cell").css("height", CELL_SIZE + "px");
 });
 
+function AddBrushes() {
+  for (var key in BRUSH_TYPES) {
+    var brush = $("<li>", {
+      "text": key,
+      "class": "brush",
+      "onClick": "OnBrushClicked(this)",
+      css: {
+        color: BRUSH_TYPES[key].textcolor,
+        backgroundColor: BRUSH_TYPES[key].color
+      }
+    });
+    $("#brushes").append(brush);
+  }
+}
+
+function OnBrushClicked(obj) {
+  // var brushType = BrushTypes[$(brush).text()];
+  // console.log(brushType);
+  SetBrush(obj);
+}
+
+$(window).mousedown(function () {
+  IsMouseDown = true;
+});
+
+$(window).mouseup(function () {
+  IsMouseDown = false;
+});
+
+
 function SpawnCells() {
   for (x = 0; x < W_CELLS; x++) {
     for (y = 0; y < H_CELLS; y++) {
       var cell = $("<div>", {
         "class": "cell",
-        "style": "left: " + (CELL_SIZE * x) + "px; top: " + (CELL_SIZE * y) + "px",
-        "onClick": "OnCellClicked()"
+        "onMouseEnter": `OnCellClicked(this, ${x}, ${y})`,
+        css: {
+          left: (CELL_SIZE * x) + "px",
+          top: (CELL_SIZE * y) + "px"
+        }
       });
       $("#grid").append(cell);
     }
   }
 }
 
-function OnCellClicked(x, y) {
-  console.log(x + " " + y);
+function OnCellClicked(obj, x, y) {
+  // console.log(cell);
+  // console.log(x + " " + y);
+  if (IsMouseDown === true) {
+    $(obj).css("background-color", "red");
+  }
+}
+
+function SetBrush(brushType) {
+  console.log(brushType);
 }
